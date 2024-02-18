143aa42292d66af85d43394dd71b04c103ad01fa
fluxmyria <136126566+fluxmyria@users.noreply.github.com>
2024-02-18 16:06:15 +0000

    Update fluxinsert.sh

--- fluxinsert.sh
+++ fluxinsert.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
- bash
+bash
 
-sed -i 's/key/$key/g' /etc/sysconfig/myria-node-service
 myria-node --start 
